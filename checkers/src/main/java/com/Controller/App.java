64df1f58be9cf2ac2de644ad333910d9ebc8a240
VictorGhosh <ghoshvictor215@gmail.com>
2024-05-13 12:14:17 -0400

    Fixed issue with not being able to click on game pieces. New bug where sometimes failed move leaves square highlighted Expected issue clicking on Kings crown (don't bother fixing until crown image is in) Still haveing problems with king movement.

--- checkers/src/main/java/com/Controller/App.java
+++ checkers/src/main/java/com/Controller/App.java
@@ -1,9 +1,15 @@
 package com.Controller;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -40,8 +46,8 @@ public class App extends Application {
     public void start(Stage primaryStage) {
         this.root = new BorderPane();
         this.boardPane = new Pane();
-        initializeBoard();
-        initializeSidebar();
+        this.p1Name = "Player 1";
+        this.p2Name = "Player 2";
         root.setLeft(boardPane);
 
         Scene scene = new Scene(root, 1000, 800);
@@ -49,15 +55,19 @@ public class App extends Application {
         primaryStage.setTitle("Checkers");
         primaryStage.setScene(scene);
 
+        initializeBoard();
+        initializeSidebar();
         updateBoard();
         updateSidebar();
+
         primaryStage.show();
     }
 
+    /**
+     * Initalizes new gameboard using attribute player names. Creates game object and board
+     * including each square and sets action for when squares are clicked. 
+     */
     public void initializeBoard() {
-        // FIXME: Move these
-        this.p1Name = "Player 1";
-        this.p2Name = "Player 2";
         this.game = new Game(p1Name, p2Name);
         game.initialize();
 
@@ -65,16 +75,18 @@ public class App extends Application {
             for (int j = 0; j < 8; j++) {
                 Rectangle square = new Rectangle(scale * j, scale * i, scale, scale);
                 square.setFill((i + j) % 2 == 0 ? Color.WHITE : Color.GRAY);
-                root.getChildren().add(square);
+                boardPane.getChildren().add(square);
 
-                square.setOnMouseClicked(event -> squareClicked(event, square));
+                square.setOnMouseClicked(event -> onSquareClick(event, square));
             }
         }
     }
 
+    /**
+     * Initialize sidebar layout. Make design changes here
+     */
     private void initializeSidebar() {
         VBox sidebar = new VBox(10);
-        // sidebar.setPadding(new Insets(15));
         sidebar.getStyleClass().add("sidebar");
 
         turnLabel = new Label();
@@ -83,13 +95,14 @@ public class App extends Application {
         p1ScoreLabel.getStyleClass().add("score-label");
         p2ScoreLabel = new Label();
         p2ScoreLabel.getStyleClass().add("score-label");
-        
+
         sidebar.getChildren().addAll(turnLabel, p1ScoreLabel, p2ScoreLabel);
         root.setRight(sidebar);
     }
     
     /**
-     * FIXME: Redrawing all game pieces every update is not good but I'm lazy
+     * Update game peice positions on board including kings. Should be run after every move or game state
+     * change. TODO: Currently incomplete as it redraws ALL pieces every time, which is not ideal.
      */
     private void updateBoard() {
         GamePiece[][] board = game.getBoard();
@@ -120,22 +133,32 @@ public class App extends Application {
                         hole.setFill(gamePiece.getColor() == GamePiece.Color.BLACK ? Color.BLACK : Color.RED);
                         root.getChildren().add(hole);
                     }
+                    piece.setOnMouseClicked(event -> onCircleClick(event));
                 }
             }
         }
     }
     
+    /**
+     * Update sidebar values from game object. 
+     */
     private void updateSidebar() {
         turnLabel.setText(game.getCurrentPlayer().getName() + "'s Turn");
-        
+
         p1ScoreLabel.setText(game.getPlayers()[0].getName() + ": "
                 + game.countPieces(game.getPlayers()[0].getColor()) + " pieces");
         p2ScoreLabel.setText(game.getPlayers()[1].getName() + ": "
                 + game.countPieces(game.getPlayers()[1].getColor()) + " pieces");
     }
-
-
-    private void squareClicked(javafx.scene.input.MouseEvent event, Rectangle square) {
+    
+    /**
+     * When a square is clicked first checks if it is the start or end position for the move.
+     * If start, highlights the square, if end execute the move and reset. Calls for updates.
+     * 
+     * @param event mouseclick event to find piece posisiton
+     * @param square the square that was clicked on
+     */
+    private void onSquareClick(javafx.scene.input.MouseEvent event, Rectangle square) {
         if (initRow < 0 && initCol < 0) {
             this.initRow = (int) Math.round(event.getSceneY()) / 100;
             this.initCol = (int) Math.round(event.getSceneX()) / 100;
@@ -148,13 +171,46 @@ public class App extends Application {
             int endCol = (int) Math.round(event.getSceneX()) / 100;
 
             System.out.println("(" + initRow + ", " + initCol + ") -> (" + endRow + ", " + endCol
-                + ") " + game.takeTurn(initRow, initCol, endRow, endCol));
+                    + ") " + game.takeTurn(initRow, initCol, endRow, endCol));
             activeSquare.setStrokeWidth(0);
             this.initRow = -1;
             this.initCol = -1;
             updateBoard();
             updateSidebar();
         }
+    }
+    
+    /**
+     * When a circle is clicked find the square under it and act like that was clicked instead.
+     * 
+     * @param event the mouse click event for finding the square
+     */
+    private void onCircleClick(javafx.scene.input.MouseEvent event) {
+        onSquareClick(event, getSquareUnder(event.getSceneX(), event.getSceneY()));
+    }
+    
+    /**
+     * Returns the square located under x,y for when the circle is clicked instead. 
+     * 
+     * @param x coordinate of the target square/location
+     * @param y coordinate of the target square/location
+     * @return Rectangle object of square under postion x,y or null if no square is found.
+     */
+    private Rectangle getSquareUnder(double x, double y) {
+        int row = (int) x / 100;
+        int col = (int) y / 100;
+        for (Node node : boardPane.getChildren()) {
+            if (node instanceof Rectangle) {
+                Rectangle rect = (Rectangle) node;
+                System.out.println(rect.getX() + ", " + rect.getY());
+                if (rect.getX() == row * 100 && rect.getY() == col * 100) {
+                    rect.setStroke(Color.GOLD);
+                    rect.setStrokeWidth(5);
+                    return rect;
+                }
+            }
+        }
+        return null;
     }
     
     public static void main(String[] args) {
